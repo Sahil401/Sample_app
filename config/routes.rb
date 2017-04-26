3d27cc9b290a570d085cdc303748ca1da989fcc7
sahil chahar <sahilchahar95@gmail.com>
2017-04-26 13:20:28 +0530

    Sign up is added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SampleApp::Application.routes.draw do
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+    match '/signout', to: 'sessions#destroy', via: 'get'
+  match '/signin', to: 'sessions#new', via: 'get'
   match '/signup', to:'users#new', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
