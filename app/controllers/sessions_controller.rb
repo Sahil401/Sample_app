a0d96c06c01bb467750e4f3214c2a7d04bdacc0f
sahil chahar <sahilchahar95@gmail.com>
2017-04-26 18:12:03 +0530

    SignIn and SignOut part done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,27 @@
 class SessionsController < ApplicationController
+
 def new
+	redirect_to user_path(current_user) if current_user.present?
 end
+
 def destroy
+	sign_out
+	redirect_to root_url
 end
+
 def create
+	user=User.find_by(email: params[:session][:email])
+	if user
+		if user.authenticate(params[:session][:password])
+			sign_in user
+		redirect_to user
+		else
+			flash.now[:error]="Wrong password"
+			render 'new'
+		end
+	else
+		redirect_to signup_path
+	end	
 end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SampleApp::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
-    match '/signout', to: 'sessions#destroy', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
   match '/signin', to: 'sessions#new', via: 'get'
   match '/signup', to:'users#new', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
